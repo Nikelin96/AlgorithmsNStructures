73ffde33532c6a755b5774393371295a35116222
mrevutskyi <mrevutskyi@brightgrove.com>
2024-02-17 15:42:18 +0200

    work in progress

--- src/Program.cs
+++ src/Program.cs
@@ -14,6 +14,9 @@ using AlgorithmsNStructures.ChapterComputers;
 //var test = new[] { -10, -3, 0, 5, 9 };
 
 //var test = new[] {7,1,5,3,6,4 };
-var test = new [] { 2,2,3};
+//var test = new [] { 2,2,3};
 
-new DynamicComputer().RemoveElement(test, 3);
+//new DynamicComputer().RemoveElement(test, 3);
+
+var test = new [] { 1,2,3};
+new ArrayComputer().FindMajorityElement(test);
